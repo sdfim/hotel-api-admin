6ff6420f447d5cf5752a8067c680e516190d781b
DubovkaE <33190113+DubovkaE@users.noreply.github.com>
2023-09-19 10:04:22 +0300

    change test

--- tests/Feature/ReservationsTest.php
+++ tests/Feature/ReservationsTest.php
@@ -5,16 +5,29 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class ReservationsTest extends TestCase
 {
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    public function testListReservations(): void
     {
-        $response = $this->get('/');
+        $this->auth();
+
+        $response = $this->get('/reservations');
 
         $response->assertStatus(200);
     }
+
+    public function auth()
+	{
+		$user = User::factory()->create();
+
+		$this->post('/login', [
+			'email' => $user->email,
+			'password' => 'password',
+		]);
+	}
 }
