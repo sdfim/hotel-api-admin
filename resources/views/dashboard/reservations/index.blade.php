49802fdbc135e6e914cb0636534550001140339e
Davyd <davidyeemelyanov@gmail.com>
2023-09-26 21:12:08 +0300

    Removed extra scripts

--- resources/views/dashboard/reservations/index.blade.php
+++ resources/views/dashboard/reservations/index.blade.php
@@ -16,27 +16,3 @@
         </div>
     </div>
 @endsection
-@section('scripts')
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"
-            integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-
-    <!-- Required datatable js -->
-    <script src="{{ URL::asset('build/libs/datatables.net/js/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ URL::asset('build/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-    <!-- Buttons examples -->
-    <script src="{{ URL::asset('build/libs/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
-    <script src="{{ URL::asset('build/libs/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js') }}"></script>
-    <script src="{{ URL::asset('build/libs/jszip/jszip.min.js') }}"></script>
-    <script src="{{ URL::asset('build/libs/pdfmake/build/pdfmake.min.js') }}"></script>
-    <script src="{{ URL::asset('build/libs/pdfmake/build/vfs_fonts.js') }}"></script>
-    <script src="{{ URL::asset('build/libs/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
-    <script src="{{ URL::asset('build/libs/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
-    <script src="{{ URL::asset('build/libs/datatables.net-buttons/js/buttons.colVis.min.js') }}"></script>
-
-    <!-- Responsive examples -->
-    <script src="{{ URL::asset('build/libs/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
-    <script src="{{ URL::asset('build/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js') }}"></script>
-
-    <!-- Datatable init js -->
-    <script src="{{ URL::asset('build/js/pages/datatables.init.js') }}"></script>
-@endsection
