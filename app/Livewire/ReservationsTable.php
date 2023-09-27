fbc900f824cf52e4ba42985d83d3c2f88a89571b
DubovkaE <33190113+DubovkaE@users.noreply.github.com>
2023-09-27 08:42:45 +0300

    change reservations datatable

--- app/Livewire/ReservationsTable.php
+++ app/Livewire/ReservationsTable.php
@@ -12,6 +12,12 @@ use Filament\Tables\Table;
 use Livewire\Component;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Builder;
+use Filament\Tables\Actions\DeleteAction;
+use Filament\Tables\Actions\EditAction;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Filters\Filter;
+
 
 class ReservationsTable extends Component implements HasForms, HasTable
 {
@@ -21,42 +27,53 @@ class ReservationsTable extends Component implements HasForms, HasTable
     public function table(Table $table): Table
     {
         return $table
-            ->query(Reservations::query())
+            ->query(Reservations::query()->whereNull('canceled_at'))
             ->columns([
-                Tables\Columns\TextColumn::make('contains_id')
+                Tables\Columns\ViewColumn::make('contains.name')->searchable()->view('components.datatable-contains-column'),
+                
+                Tables\Columns\TextColumn::make('channel.name')
                     ->numeric()
-                    ->sortable(),
-                Tables\Columns\TextColumn::make('channel_id')
-                    ->numeric()
+                    ->searchable()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('date_offload')
                     ->dateTime()
+                    ->searchable()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('date_travel')
                     ->dateTime()
+                    ->searchable()
                     ->sortable(),
                 Tables\Columns\TextColumn::make('passenger_surname')
                     ->searchable(),
                 Tables\Columns\TextColumn::make('total_cost')
                     ->numeric()
+                    ->searchable()
+                    ->money('USD')
                     ->sortable(),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
+                    ->searchable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('updated_at')
                     ->dateTime()
                     ->sortable()
+                    ->searchable()
                     ->toggleable(isToggledHiddenByDefault: true),
                 Tables\Columns\TextColumn::make('canceled_at')
                     ->dateTime()
-                    ->sortable(),
+                    ->sortable()
+                    ->searchable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\ViewColumn::make('id')->searchable()->view('components.datatable-reservations-actions-column'),
+                
+
             ])
             ->filters([
                 //
             ])
             ->actions([
-                //
+                
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
