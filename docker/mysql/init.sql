c1a0f7cb31df20ba3495efd7728d02547e610268
Davyd <davidyeemelyanov@gmail.com>
2023-09-28 16:22:32 +0300

    updated docker-compose.local.yml with initial script for creation 3 dbs

---
+++ docker/mysql/init.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE ujv;
+GRANT ALL PRIVILEGES ON ujv.* TO 'admin'@'%';
+
+CREATE DATABASE ujv_test;
+GRANT ALL PRIVILEGES ON ujv_test.* TO 'admin'@'%';
+
+CREATE DATABASE ujv_aws_test;
+GRANT ALL PRIVILEGES ON ujv_aws_test.* TO 'admin'@'%';
