b45266b693833d47c56bdb5c2e99ef796e6381c6
andrii <kslndr@gmail.com>
2023-10-01 22:39:33 +0300

    up migrate for test sqlite

--- database/migrations/2023_09_27_203240_create_mapper_expedia_giatas_table.php
+++ database/migrations/2023_09_27_203240_create_mapper_expedia_giatas_table.php
@@ -10,16 +10,18 @@ return new class extends Migration {
      */
     public function up (): void
     {
-		// if (!Schema::connection(env('DB_CONNECTION_2', 'mysql2'))->hasTable('mapper_expedia_giatas')) {
+		if (!Schema::connection(env('DB_CONNECTION_2', 'mysql2'))->hasTable('mapper_expedia_giatas')) {
             Schema::connection(env('DB_CONNECTION_2', 'mysql2'))->create('mapper_expedia_giatas', function (Blueprint $table) {
 				$table->id();
 				$table->integer('expedia_id');
 				$table->integer('giata_id');
-				$table->foreign('giata_id')->references('code')->on(env('SECOND_DB_DATABASE', 'ujv_api').'.giata_properties')->onDelete('cascade');
-				$table->foreign('expedia_id')->references('property_id')->on(env('SECOND_DB_DATABASE', 'ujv_api').'.expedia_contents')->onDelete('cascade');
+				// $table->foreign('giata_id')->references('code')->on(env('SECOND_DB_DATABASE', 'ujv_api').'.giata_properties')->onDelete('cascade');
+				// $table->foreign('expedia_id')->references('property_id')->on(env('SECOND_DB_DATABASE', 'ujv_api').'.expedia_contents')->onDelete('cascade');
+				// $table->connection(env('DB_CONNECTION_2', 'mysql2'))->foreign('giata_id')->references('code')->on('giata_properties')->onDelete('cascade');
+				// $table->connection(env('DB_CONNECTION_2', 'mysql2'))->foreign('expedia_id')->references('property_id')->on('expedia_contents')->onDelete('cascade');
 				$table->integer('step');
 			});
-		// }
+		}
     }
 
     /**
