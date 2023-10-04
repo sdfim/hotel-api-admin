36ba57051cbbf7ba858383062e939689f376d2fe
andrii <kslndr@gmail.com>
2023-10-04 22:49:54 +0300

    feat: add model and migration inspector, up console command mapper

--- Modules/Inspector/InspectorController.php
+++ Modules/Inspector/InspectorController.php
@@ -2,16 +2,48 @@
 
 namespace Modules\Inspector;
 
-
-use Illuminate\Support\Facades\Cache;
-
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Support\Facades\Storage;
+use App\Models\Channels;
+use App\Models\ApiInspector;
+use Illuminate\Support\Str;
 class InspectorController
 {
-	public function save()
+	public function save($query, $content, $supplier_id , $type = 'search') : string|bool
 	{
-		$value = Cache::get('dataPriceAll');
-		$data = json_decode($value);
-		return view('inspector::index', ['data' => $data]);
+		try {
+			$ch = new Channels;
+			$token_id = $ch->getTokenId(request()->bearerToken());
+			$query = json_encode($query);
+			$content = json_encode($content);
+			$hash = md5($query);
+			$path = $type. '/' . $hash.'.json';
+
+			$inspector = ApiInspector::where('response_path', $path)->first();
+			if ($inspector) return $inspector->id;
+
+			Storage::put($path, $content);
+
+			$uuid = Str::uuid()->toString();
+
+			$data = [
+				'id' => $uuid,
+				'token_id' => $token_id,
+				'supplier_id' => $supplier_id,
+				'type' => $type,
+				'request' => $query,
+				'response_path' => $path
+			];
+
+			$inspector = ApiInspector::create($data);
+
+			return $inspector ? $uuid : false;
+
+		} catch (\Exception $e) {
+            \Log::error('Error save ApiInspector: ' . $e->getMessage(). ' | ' . $e->getLine() . ' | ' . $e->getFile());
+			
+			return false;
+		}
 	}
 
 	public function get()
---
+++ app/Models/ApiInspector.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+
+class ApiInspector extends Model
+{
+    use HasFactory;
+
+	public $incrementing = false;
+    protected $keyType = 'string';
+    protected $guarded = ['id'];
+
+	protected $fillable = [
+		'id',
+		'token_id',
+		'supplier_id',
+		'type',
+		'request',
+		'response_path'
+	];
+
+	protected static function booted(): void
+    {
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = Str::uuid()->toString();
+        });
+    }
+
+}
