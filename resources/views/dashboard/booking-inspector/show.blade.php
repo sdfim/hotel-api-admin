0af5855846b6b44a1641f919463a5a86340bec37
andrii <kslndr@gmail.com>
2023-10-13 10:02:50 +0300

    update views

--- resources/views/dashboard/booking-inspector/show.blade.php
+++ resources/views/dashboard/booking-inspector/show.blade.php
@@ -13,20 +13,40 @@
 
     <div class="col-span-12 xl:col-span-6">
         <div class="card dark:bg-zinc-800 dark:border-zinc-600">
-            <div class="card-body pb-0">
-                <h6 class="mb-1 text-15 text-gray-700 dark:text-gray-100" x-data="{ message: '{{ $text['show'] }}' }"
-                    x-text="message"></h6>
+            <div class="card-body pb-0 flex">
+				<x-button-back route="{{ redirect()->getUrlGenerator()->previous() }}" text="Back"/>
+                <h6 class="mb-1 text-15 text-gray-700 dark:text-gray-100" >Booking Inspector</h6>
             </div>
             <div class="card-body">
                 <div class="relative overflow-x-auto">
                     <div class="row">
                         <div class="col-lg-12 margin-tb">
-                            <div class="pull-left">
-                                <h2 x-data="{ message: '{{ $text['show'] }}' }" x-text="message"></h2>
+							<div class="mt-2">
+								<strong>Booking ID:</strong>
+                                {{ $inspector->booking_id }}
                             </div>
-                            <div class="mt-6 mb-6">
-                                <x-button-back route="{{ redirect()->getUrlGenerator()->previous() }}" text="Back"/>
-                            </div>
+							<div class="mt-2">
+								<strong>Search ID:</strong>
+								{{ $inspector->search_id }}
+							</div>
+							<div class="mt-2">
+								<strong>Search Type:</strong>
+								{{ $inspector->search_type }}
+							</div>
+							<div class="mt-2">
+								<strong>Endpoint:</strong>
+								{{ $inspector->type }} 
+								<strong>step:</strong>
+								{{ $inspector->sub_type }}
+							</div>
+							<div class="mt-2">
+								<strong>Supplier:</strong>
+								{{ $inspector->Supplier->name }}
+							</div>
+							<div class="mt-2">
+								<strong>Channel:</strong>
+								{{ $inspector->token_id }}
+							</div>
                         </div>
                     </div>
                     <div class="mt-10 sm:mt-0">
@@ -38,11 +58,11 @@
                     <ul class="flex flex-wrap w-full text-sm font-medium text-center text-gray-500 nav">
                         <li>
                             <a href="javascript:void(0);" data-tw-toggle="tab" data-tw-target="tab-pills-response"
-                                class="inline-block px-4 py-3 rounded-md active">Response</a>
+                                class="inline-block px-4 py-3 rounded-md active">Supplier Response</a>
                         </li>
                         <li>
                             <a href="javascript:void(0);" data-tw-toggle="tab" data-tw-target="tab-pills-client-response"
-                                class="inline-block px-4 py-3 rounded-md dark:hover:text-white">Client Response</a>
+                                class="inline-block px-4 py-3 rounded-md dark:hover:text-white">UJV API Response</a>
                         </li>
                         
                     </ul>
@@ -62,7 +82,7 @@
                                     <button class="btn text-white bg-gray-500 border-gray-500 hover:bg-gray-600 hover:border-gray-600 focus:bg-gray-600 focus:border-gray-600 focus:ring focus:ring-gray-500/30 active:bg-gray-600 active:border-gray-600" id="collapse-response">Collapse All</button>
                                     <input class="rounded border-gray-100 py-2.5 text-sm text-gray-500 focus:border focus:border-violet-500 focus:ring-0 dark:bg-zinc-700/50 dark:border-zinc-600 dark:text-zinc-100" id="search-response" placeholder="search"></input>
                                 </div>
-                                <json-viewer id="json-response"></json-viewer>
+                                <json-viewer id="json-response" style="font-size:0.8em"></json-viewer>
                             </p>
                         </div>
                         <div class="hidden tab-pane" id="tab-pills-client-response">
@@ -79,7 +99,7 @@
                                     <button class="btn text-white bg-gray-500 border-gray-500 hover:bg-gray-600 hover:border-gray-600 focus:bg-gray-600 focus:border-gray-600 focus:ring focus:ring-gray-500/30 active:bg-gray-600 active:border-gray-600" id="collapse-client">Collapse All</button>
                                     <input class="rounded border-gray-100 py-2.5 text-sm text-gray-500 focus:border focus:border-violet-500 focus:ring-0 dark:bg-zinc-700/50 dark:border-zinc-600 dark:text-zinc-100" id="search-client" placeholder="search"></input>
                                 </div>
-                                <json-viewer id="json-client"></json-viewer>
+                                <json-viewer id="json-client" style="font-size:0.8em"></json-viewer>
                             </p>
                         </div>
                     </div>
