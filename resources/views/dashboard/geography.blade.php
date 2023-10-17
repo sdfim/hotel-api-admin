f937f4d7bdaf0be32fa99fce5c6f5177086f89c0
DubovkaE <33190113+DubovkaE@users.noreply.github.com>
2023-10-17 11:50:27 +0300

    add datatable and filters in geography page

--- resources/views/dashboard/geography.blade.php
+++ resources/views/dashboard/geography.blade.php
@@ -14,6 +14,15 @@
     <!-- -->
     <x-page-title title="Geography" pagetitle="index"/>
 
+    <div class="grid grid-cols-12 gap-5">
+        <div class="col-span-12">
+            <div class="card dark:bg-zinc-800 dark:border-zinc-600">
+                <div class="card-body relative overflow-x-auto">
+                    @livewire('giata-geography-table')
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 @section('scripts')
 @endsection
