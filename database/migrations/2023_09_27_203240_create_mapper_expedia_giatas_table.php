429dfb59d4f459fb54103aa2b7afa485367b8597
andrii <kslndr@gmail.com>
2023-10-19 19:14:29 +0300

    up migration

--- database/migrations/2023_09_27_203240_create_mapper_expedia_giatas_table.php
+++ database/migrations/2023_09_27_203240_create_mapper_expedia_giatas_table.php
@@ -18,10 +18,10 @@ return new class extends Migration
 				$table->id();
 				$table->integer('expedia_id');
 				$table->integer('giata_id');
+
 				$table->foreign('giata_id')->references('code')->on(env('SECOND_DB_DATABASE', 'ujv_api').'.giata_properties')->onDelete('cascade');
 				$table->foreign('expedia_id')->references('property_id')->on(env('SECOND_DB_DATABASE', 'ujv_api').'.expedia_contents')->onDelete('cascade');
-				$table->connection($connection)->foreign('giata_id')->references('code')->on('giata_properties')->onDelete('cascade');
-				$table->connection($connection)->foreign('expedia_id')->references('property_id')->on('expedia_contents')->onDelete('cascade');
+				
 				$table->integer('step');
 			});
 		}
