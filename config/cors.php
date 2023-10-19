5a43b799492e09d48bd5fed228889fb498bfacc3
andrii <kslndr@gmail.com>
2023-10-19 16:50:22 +0300

    up cros

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://ddwlx1ki3fks2.cloudfront.net'],
 
     'allowed_origins_patterns' => [],
 
