4388d89418bca1448738aa5d8db1e149951dde27
Davyd <davidyeemelyanov@gmail.com>
2023-10-24 19:23:21 +0300

    Seeders code cleaned and formatted.

--- Modules/Inspector/BaseInspectorController.php
+++ Modules/Inspector/BaseInspectorController.php
@@ -4,8 +4,15 @@ namespace Modules\Inspector;
 
 class BaseInspectorController
 {
-	protected $current_time;
-	public function executionTime ()
+    /**
+     * @var string|float
+     */
+    protected string|float $current_time;
+
+    /**
+     * @return string|float
+     */
+    public function executionTime (): string|float
     {
         $execution_time = (microtime(true) - $this->current_time);
         $this->current_time = microtime(true);
