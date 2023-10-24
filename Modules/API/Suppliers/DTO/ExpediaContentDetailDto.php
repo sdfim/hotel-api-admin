3de0274d4e1a7e9f9d88a89094d4c0583ac03759
andrii <kslndr@gmail.com>
2023-10-24 19:51:51 +0300

    up ExpediaToContentDetailResponse add key request category_amenities

--- Modules/API/Suppliers/DTO/ExpediaContentDetailDto.php
+++ Modules/API/Suppliers/DTO/ExpediaContentDetailDto.php
@@ -16,6 +16,7 @@ class ExpediaContentDetailDto
 		foreach ($supplierResponse->images as $image) {
 			$hotelImages[] = $image['links']['350px']['href'];
 		}
+		$viewAmenities = request()->get('category_amenities') === 'true' ? true : false;
 
 		$hotelResponse = new ContentDetailResponse();
 		$hotelResponse->setGiataHotelCode($giata_id);
@@ -26,9 +27,13 @@ class ExpediaContentDetailDto
 		$hotelResponse->setLatitude($supplierResponse->location['coordinates']['latitude']);
 		$hotelResponse->setLongitude($supplierResponse->location['coordinates']['longitude']);
 		$hotelResponse->setRating($supplierResponse->rating);
-		$hotelResponse->setAmenities($supplierResponse->amenities ? array_map(function ($amenity) {
+		if ($viewAmenities) {
+			$hotelResponse->setAmenities($supplierResponse->amenities ?? []);
+		} else {
+			$hotelResponse->setAmenities($supplierResponse->amenities ? array_map(function ($amenity) {
 				return $amenity['name'];
 			}, $supplierResponse->amenities) : []);
+		}
 		$hotelResponse->setGiataDestination($supplierResponse->city ?? '');
 		$hotelResponse->setUserRating($supplierResponse->rating ?? '');
 		$hotelResponse->setSpecialInstructions($supplierResponse->room ?? []);
@@ -40,8 +45,6 @@ class ExpediaContentDetailDto
 
 		$rooms = [];
 		foreach ($supplierResponse->rooms as $room) {
-
-			// dd($room['images']);
 			$amenities = $room['amenities'] ?? [];
 			$images = [];
 			foreach ($room['images'] as $image) {
@@ -50,9 +53,13 @@ class ExpediaContentDetailDto
 			$roomResponse = new ContentDetailRoomsResponse();
 			$roomResponse->setSupplierRoomId($room['id']);
 			$roomResponse->setSupplierRoomName($room['name']);
-			$roomResponse->setAmenities($room['amenities'] ? array_map(function ($amenity) {
-				return $amenity['name'];
-			}, $amenities) : []);
+			if ($viewAmenities) {
+				$roomResponse->setAmenities($room['amenities'] ?? []);
+			} else {
+				$roomResponse->setAmenities($room['amenities'] ? array_map(function ($amenity) {
+					return $amenity['name'];
+				}, $amenities) : []);
+			}
 			$roomResponse->setImages($images);
 			$roomResponse->setDescriptions($room['descriptions'] ? $room['descriptions']['overview'] : '');
 			$rooms[] = $roomResponse->toArray();
