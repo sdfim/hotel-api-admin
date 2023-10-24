4efeef5c778b8c566dafb70d22bf4cbf22de50e6
Davyd <davidyeemelyanov@gmail.com>
2023-10-24 15:36:24 +0300

    General configuration updated

--- resources/views/dashboard/general-configuration.blade.php
+++ resources/views/dashboard/general-configuration.blade.php
@@ -2,13 +2,6 @@
 @section('title')
     {{ __('General Configuration') }}
 @endsection
-@section('css')
-    <link href="{{ URL::asset('build/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css') }}"
-          rel="stylesheet"
-          type="text/css">
-
-    <link href="{{ URL::asset('build/libs/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
-@endsection
 @section('content')
     <!-- -->
     <x-page-title title="General Configuration" pagetitle="index"/>
