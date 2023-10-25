bff14a62793a51238c0023c64d772340446586ee
Davyd <davidyeemelyanov@gmail.com>
2023-10-25 17:07:29 +0300

    EnrichmentWeight cleaned and formatted.

--- Modules/API/PropertyWeighting/EnrichmentWeight.php
+++ Modules/API/PropertyWeighting/EnrichmentWeight.php
@@ -3,69 +3,83 @@
 namespace Modules\API\PropertyWeighting;
 
 use App\Models\PropertyWeighting;
-use App\Models\Supplier;
 
-class EnrichmentWeight 
+class EnrichmentWeight
 {
-	protected $current_time;
+    /**
+     * @var float|string
+     */
+    protected float|string $current_time;
 
-	public function enrichmentContent(array $clientResponse, string $type): array
-	{
-		$this->executionTime();
+    /**
+     * @param array $clientResponse
+     * @param string $type
+     * @return array
+     */
+    public function enrichmentContent(array $clientResponse, string $type): array
+    {
+        $this->executionTime();
 
-		$weitghts = PropertyWeighting::where('supplier_id', null)->get();
-		
-		$weitghtsProps= $weitghts->pluck('property')->toArray();
-		$weitghtsVol = $weitghts->pluck('weight', 'property')->toArray();
+        $weights = PropertyWeighting::where('supplier_id', null)->get();
+        $weightsProps = $weights->pluck('property')->toArray();
+        $weightsVol = $weights->pluck('weight', 'property')->toArray();
 
-		foreach ($clientResponse as $supllierName => $supplierResponse) {
-			foreach ($supplierResponse as $key => $hotelData) {
-				if (in_array($hotelData['giata_hotel_code'], $weitghtsProps)) {
-					$clientResponse[$supllierName][$key]['weight'] = $weitghtsVol[$hotelData['giata_hotel_code']];
-				} else {
-					$clientResponse[$supllierName][$key]['weight'] = 0;
-				}
-			}
-		}
+        foreach ($clientResponse as $supplierName => $supplierResponse) {
+            foreach ($supplierResponse as $key => $hotelData) {
+                if (in_array($hotelData['giata_hotel_code'], $weightsProps)) {
+                    $clientResponse[$supplierName][$key]['weight'] = $weightsVol[$hotelData['giata_hotel_code']];
+                } else {
+                    $clientResponse[$supplierName][$key]['weight'] = 0;
+                }
+            }
+        }
 
-		\Log::info('EnrichmentWeight | enrichmentContent  - ' . $this->executionTime() . 's');
+        \Log::info('EnrichmentWeight | enrichmentContent  - ' . $this->executionTime() . 's');
 
-		return $clientResponse;
-	}
+        return $clientResponse;
+    }
 
-	public function enrichmentPricing(array $clientResponse, string $type): array
-	{
-		$this->executionTime();
+    /**
+     * @param array $clientResponse
+     * @param string $type
+     * @return array
+     */
+    public function enrichmentPricing(array $clientResponse, string $type): array
+    {
+        $this->executionTime();
 
-		# step1 !isset supplier_id
-		$s1weitghts = PropertyWeighting::where('supplier_id', null)->get();
-		$s1weitghtsProps= $s1weitghts->pluck('property')->toArray();
-		$s1weitghtsVol = $s1weitghts->pluck('weight', 'property')->toArray();
+        # step1 !isset supplier_id
+        $s1Weights = PropertyWeighting::where('supplier_id', null)->get();
+        $s1WeightsProps = $s1Weights->pluck('property')->toArray();
+        $s1WeightsVol = $s1Weights->pluck('weight', 'property')->toArray();
 
-		# step2 isset supplier_id
-		$s2weitghts = PropertyWeighting::whereNot('supplier_id', null)->get();
-		$s2weitghtsProps= $s2weitghts->pluck('property')->toArray();
-		$s2weitghtsVol = $s2weitghts->pluck('weight', 'property')->toArray();
+        # step2 isset supplier_id
+        $s2Weights = PropertyWeighting::whereNot('supplier_id', null)->get();
+        $s2WeightsProps = $s2Weights->pluck('property')->toArray();
+        $s2WeightsVol = $s2Weights->pluck('weight', 'property')->toArray();
 
-		foreach ($clientResponse as $supllierName => $supplierResponse) {
-			foreach ($supplierResponse as $key => $hotelData) {
-				if (in_array($hotelData['giata_hotel_id'], $s1weitghtsProps)) {
-					$clientResponse[$supllierName][$key]['weight'] = $s1weitghtsVol[$hotelData['giata_hotel_id']];
-				} else {
-					$clientResponse[$supllierName][$key]['weight'] = 0;
-				}
-				if (in_array($hotelData['giata_hotel_id'], $s2weitghtsProps)) {
-					$clientResponse[$supllierName][$key]['weight'] = $s2weitghtsVol[$hotelData['giata_hotel_id']];
-				} 
-			}
-		}
+        foreach ($clientResponse as $supplierName => $supplierResponse) {
+            foreach ($supplierResponse as $key => $hotelData) {
+                if (in_array($hotelData['giata_hotel_id'], $s1WeightsProps)) {
+                    $clientResponse[$supplierName][$key]['weight'] = $s1WeightsVol[$hotelData['giata_hotel_id']];
+                } else {
+                    $clientResponse[$supplierName][$key]['weight'] = 0;
+                }
+                if (in_array($hotelData['giata_hotel_id'], $s2WeightsProps)) {
+                    $clientResponse[$supplierName][$key]['weight'] = $s2WeightsVol[$hotelData['giata_hotel_id']];
+                }
+            }
+        }
 
-		\Log::info('EnrichmentWeight | enrichmentPricing - ' . $this->executionTime() . 's');
+        \Log::info('EnrichmentWeight | enrichmentPricing - ' . $this->executionTime() . 's');
 
-		return $clientResponse;
-	}
+        return $clientResponse;
+    }
 
-	private function executionTime () : float
+    /**
+     * @return float
+     */
+    private function executionTime(): float
     {
         $execution_time = round((microtime(true) - $this->current_time), 3);
         $this->current_time = microtime(true);
