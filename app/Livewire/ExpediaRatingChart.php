c35dab88dc34b8f135825c454a632d40444419b6
Davyd <davidyeemelyanov@gmail.com>
2023-11-01 12:10:30 +0200

    Remove caching from charts, code formatted, added PHPDoc Blocks

--- app/Livewire/ExpediaRatingChart.php
+++ app/Livewire/ExpediaRatingChart.php
@@ -4,58 +4,60 @@ namespace App\Livewire;
 
 use App\Models\ExpediaContent;
 use Filament\Widgets\ChartWidget;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 
 class ExpediaRatingChart extends ChartWidget
 {
-	protected static ?string $heading = 'Expedia Rating Chart';
+    /**
+     * @var string|null
+     */
+    protected static ?string $heading = 'Expedia Rating Chart';
 
+    /**
+     * @var string|null
+     */
     protected static ?string $pollingInterval = null;
 
-	protected function getData(): array
-	{
-		$keyExpediaRatingChart = 'ExpediaRatingChart';
+    /**
+     * @var string|null
+     */
+    protected static ?string $maxHeight = '300px';
 
-		if (Cache::has($keyExpediaRatingChart . ':labels') && Cache::has($keyExpediaRatingChart . ':data')) {
+    /**
+     * @return array
+     */
+    protected function getData(): array
+    {
+        $model = ExpediaContent::select('rating', DB::raw('count(*) as total'))
+            ->groupBy('rating')
+            ->orderBy('rating', 'DESC')
+            ->get();
 
-			$labels = Cache::get($keyExpediaRatingChart . ':labels');
-			$data = Cache::get($keyExpediaRatingChart . ':data');
+        $labels = $model->pluck('rating');
+        $data = $model->pluck('total');
 
-		} else {
+        $colors = [];
+        for ($i = 0; $i < count($data); $i++) {
+            $randomColor = '#' . dechex(mt_rand(0x000000, 0xFFFFFF));
+            $colors[] = $randomColor;
+        }
 
-			$model = ExpediaContent::select('rating', DB::raw('count(*) as total'))
-				->groupBy('rating')
-				->orderBy('rating', 'DESC')
-				->get();
+        return [
+            'datasets' => [
+                [
+                    'data' => $data,
+                    'backgroundColor' => $colors,
+                ],
+            ],
+            'labels' => $labels,
+        ];
+    }
 
-			$labels = $model->pluck('rating');
-			$data = $model->pluck('total');
-
-			Cache::put($keyExpediaRatingChart . ':labels', $labels, now()->addMinutes(1440));
-			Cache::put($keyExpediaRatingChart . ':data', $data, now()->addMinutes(1440));
-
-		}
-
-		$colors = [];
-		for ($i = 0; $i < count($data); $i++) {
-			$randomColor = '#' . dechex(mt_rand(0x000000, 0xFFFFFF));
-			$colors[] = $randomColor;
-		}
-
-		return [
-			'datasets' => [
-				[
-					'data' => $data ,
-					'backgroundColor' => $colors,
-				],
-			],
-			'labels' => $labels,
-		];
-	}
-
-	protected function getType(): string
-	{
-		return 'doughnut';
-	}
+    /**
+     * @return string
+     */
+    protected function getType(): string
+    {
+        return 'doughnut';
+    }
 }
--- app/Livewire/GiataCityChart.php
+++ app/Livewire/GiataCityChart.php
@@ -4,58 +4,60 @@ namespace App\Livewire;
 
 use App\Models\GiataProperty;
 use Filament\Widgets\ChartWidget;
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 
 class GiataCityChart extends ChartWidget
 {
+    /**
+     * @var string|null
+     */
     protected static ?string $heading = 'Giata City Chart';
 
+    /**
+     * @var string|null
+     */
     protected static ?string $pollingInterval = null;
 
+    /**
+     * @var string|null
+     */
+    protected static ?string $maxHeight = '300px';
+
+    /**
+     * @return array
+     */
     protected function getData(): array
-	{
-		$keyGiataCityChart = 'GiataCityChart';
+    {
+        $model = GiataProperty::with('giataGeography')
+            ->select('city_id', 'city', DB::raw('count(*) as total'))
+            ->groupBy('city_id', 'city')
+            ->orderBy('total', 'DESC')
+            ->take(10)
+            ->get();
 
-		if (Cache::has($keyGiataCityChart . ':labels') && Cache::has($keyGiataCityChart . ':data')) {
+        $labels = $model->pluck('city');
+        $data = $model->pluck('total');
 
-			$labels = Cache::get($keyGiataCityChart . ':labels');
-			$data = Cache::get($keyGiataCityChart . ':data');
+        $colors = [];
+        for ($i = 0; $i < count($data); $i++) {
+            $randomColor = '#' . dechex(mt_rand(0x000000, 0xFFFFFF));
+            $colors[] = $randomColor;
+        }
 
-		} else {
+        return [
+            'datasets' => [
+                [
+                    'data' => $data,
+                    'backgroundColor' => $colors,
+                ],
+            ],
+            'labels' => $labels,
+        ];
+    }
 
-			$model = GiataProperty::with('giataGeography')
-				->select('city_id', 'city', DB::raw('count(*) as total'))
-				->groupBy('city_id', 'city')
-				->orderBy('total', 'DESC')
-				->take(10)
-				->get();
-
-			$labels = $model->pluck('city');
-			$data = $model->pluck('total');
-
-			Cache::put($keyGiataCityChart . ':labels', $labels, now()->addMinutes(1440));
-			Cache::put($keyGiataCityChart . ':data', $data, now()->addMinutes(1440));
-
-		}
-
-		$colors = [];
-		for ($i = 0; $i < count($data); $i++) {
-			$randomColor = '#' . dechex(mt_rand(0x000000, 0xFFFFFF));
-			$colors[] = $randomColor;
-		}
-
-		return [
-			'datasets' => [
-				[
-					'data' => $data ,
-					'backgroundColor' => $colors,
-				],
-			],
-			'labels' => $labels,
-		];
-	}
-
+    /**
+     * @return string
+     */
     protected function getType(): string
     {
         return 'doughnut';
