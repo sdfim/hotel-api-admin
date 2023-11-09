f397340af215e18ebea786c79ee81a4b69b73d22
andrii <kslndr@gmail.com>
2023-11-09 12:41:47 +0200

    up HotelBookingBookTest

--- tests/Feature/API/Booking/HotelBookingBookTest.php
+++ tests/Feature/API/Booking/HotelBookingBookTest.php
@@ -2,13 +2,11 @@
 
 namespace Tests\Feature\API\Booking;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Modules\API\BookingAPI\BookingApiHandlers\BookApiHandler;
 use Tests\TestCase;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Illuminate\Testing\TestResponse;
 
 class HotelBookingBookTest extends TestCase
 {
@@ -16,18 +14,27 @@ class HotelBookingBookTest extends TestCase
 
     public function test_book_method_response()
     {
-        $request = Request::create('/book', 'POST', ['booking_id' => '1949b7ad-559e-4826-b6c9-746b94022ec7']);
-        $controller = new BookApiHandler();
+		$headers = $this->getHeader();
 
-		// Add a bearer token to the request
-        $token = 'your_bearer_token_here';
-        $request->headers->add(['Authorization' => 'Bearer ' . $token]);
+		$response = $this->withHeaders($headers)
+			->postJson('/api/booking/book', ['booking_id' => 'd491dd2b-56fc-45e5-a7e3-3ed5a9ffb023']);
 
-        $response = $controller->book($request);
+		// dump($headers);
+		// $response->dd();
 
-        $response->assertStatus(200); 
-
-        $response->assertJson(['status' => 'success']); 
-        $response->assertJsonStructure(['data']);
+		$response
+			->assertStatus(400)
+			->assertJson([
+				'success' => false,
+			]);
     }
+
+	public function getHeader() : array
+	{
+		$user = User::factory()->create();
+		$token = $user->createToken('TestToken')->plainTextToken;
+		return [
+			'Authorization' => 'Bearer ' . $token,
+		];
+	}
 }
