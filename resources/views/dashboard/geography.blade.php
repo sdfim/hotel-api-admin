d493b459ad4b7b9073cc0e41c767a2384be555ce
andrii <kslndr@gmail.com>
2023-11-16 17:50:43 +0200

    update: packege_json

--- resources/views/dashboard/geography.blade.php
+++ resources/views/dashboard/geography.blade.php
@@ -3,10 +3,6 @@
     {{ __('Geography') }}
 @endsection
 @section('css')
-    <link href="{{ URL::asset('build/libs/admin-resources/jquery.vectormap/jquery-jvectormap-1.2.2.css') }}"
-          rel="stylesheet"
-          type="text/css">
-
     <link href="{{ URL::asset('build/libs/swiper/swiper-bundle.min.css') }}" rel="stylesheet">
 @endsection
 @section('content')
