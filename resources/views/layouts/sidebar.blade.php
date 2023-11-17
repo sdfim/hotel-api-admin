b6f7352fa5c740ff5e334177b74ae4b7f87c0b37
andrii <kslndr@gmail.com>
2023-11-17 16:06:07 +0200

    update: path

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -131,7 +131,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="{{ url('admin/log-viewer') }}"
+                    <a href="{{ url('log-viewer') }}"
                        class="pl-6 pr-4 py-3 block text-sm font-medium text-gray-700 transition-all duration-150 ease-linear hover:text-violet-500 dark:text-gray-300 dark:active:text-white dark:hover:text-white">
                         <i class="dripicons-document-remove"></i>
                         <span data-key="t-log-viewer"> Log Viewer</span>
