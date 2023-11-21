38945c76bcf410cf6c9a8cccaefdb0a890a79dfd
andrii <kslndr@gmail.com>
2023-11-21 19:21:13 +0200

    up

--- resources/views/livewire/general-configuration/create-general-configuration-form.blade.php
+++ resources/views/livewire/general-configuration/create-general-configuration-form.blade.php
@@ -1,19 +1,19 @@
 <div>
-	<form wire:submit="save">
-		{{ $this->form }}
+    <form wire:submit="save">
+        {{ $this->form }}
 
-		@if ($create)
-		<x-button class="mt-4">
-			{{ __('Update') }}
-		</x-button>
-		@else
-		<x-button class="mt-4">
-			{{ __('Create') }}
-		</x-button>
-		@endif
-	</form>
+        @if ($create)
+            <x-button class="mt-4">
+                {{ __('Update') }}
+            </x-button>
+        @else
+            <x-button class="mt-4">
+                {{ __('Create') }}
+            </x-button>
+        @endif
+    </form>
 
-	<x-filament-actions::modals />
+    <x-filament-actions::modals/>
 </div>
 
 <script>
