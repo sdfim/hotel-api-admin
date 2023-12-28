424e7d61b617eb14f196652a48448e2ce8969958
andrii <kslndr@gmail.com>
2023-12-28 13:16:06 +0200

    updated crontab, used artisan schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,14 +12,14 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // # Expedia Content download archive, unzip, parse json, write to DB
-        // $schedule->command('download-expedia-data content 1')->cron('0 1 * * *');
+        # Expedia Content download archive, unzip, parse json, write to DB
+        $schedule->command('download-expedia-data content 12345')->cron('0 1 * * *');
 
-        // # GIATA Get the XML content in streming from the response body, parse XML, write to DB
-        // $schedule->command('download-giata-data')->cron('0 2 * * *');
+        $schedule->command('purge-baskets')->cron('0 1 * * *');
+        $schedule->command('purge-inspectors')->cron('0 1 * * *');
+        $schedule->command('purge-pricing-rules')->cron('0 1 * * *');
 
-        // # Mapper Expedia Giata. search for Expedia Giata relationships and write to DB
-        // $schedule->command('make-mapper-expedia-giate 1 2')->cron('0 3 * * *');
+        // $schedule->command('test-speed-db)->cron('10 * * * *');
     }
 
     /**
