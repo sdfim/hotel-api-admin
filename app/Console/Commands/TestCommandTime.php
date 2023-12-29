7542d3f9c6cabb157f020d359fc2ff22eb54cf34
andrii <kslndr@gmail.com>
2023-12-29 14:17:59 +0200

    uo cron

---
+++ app/Console/Commands/TestCommandTime.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Carbon\Carbon;
+use App\Models\ExpediaContent;
+use App\Models\User;
+
+
+class TestCommandTime extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'test-command-time';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     * @return void
+     */
+    public function handle(): void
+    {
+        $currentTime = Carbon::now('UTC');
+        $this->info('test-command-time successful, UTC: ' . $currentTime);
+
+    }
+}
