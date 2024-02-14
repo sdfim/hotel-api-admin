3721ea132e1fbc9f0a6b2fd25cf007db2c4a5f40
Davyd <davidyeemelyanov@gmail.com>
2024-02-14 16:32:52 +0200

    Removed extra elements from channel and supplier views.

--- resources/views/dashboard/suppliers/index.blade.php
+++ resources/views/dashboard/suppliers/index.blade.php
@@ -11,7 +11,6 @@
                 <a class="btn text-violet-500 hover:text-white border-violet-500 hover:bg-violet-600 hover:border-violet-600 focus:bg-violet-600 focus:text-white focus:border-violet-600 focus:ring focus:ring-violet-500/30 active:bg-violet-600 active:border-violet-600"
                    href="{{ route('suppliers.create') }}"> <i class="bx bx-plus block text-lg"></i></a>
             </div>
-
         </div>
     </div>
     <div class="grid grid-cols-12 gap-5">
