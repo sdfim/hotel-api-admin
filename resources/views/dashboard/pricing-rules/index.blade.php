11f0df6091f6b8173d65dbe36938830525628501
Davyd <davidyeemelyanov@gmail.com>
2024-02-14 17:27:55 +0200

    Removed extra elements from some views.

--- resources/views/dashboard/pricing-rules/index.blade.php
+++ resources/views/dashboard/pricing-rules/index.blade.php
@@ -7,11 +7,10 @@
     <x-page-title title="Pricing Rules" pagetitle="index"/>
     <div class="row">
         <div class="col-lg-12 margin-tb">
-            <div class="mt-6 mb-6">
+            <div class="mb-6">
                 <a class="btn text-violet-500 hover:text-white border-violet-500 hover:bg-violet-600 hover:border-violet-600 focus:bg-violet-600 focus:text-white focus:border-violet-600 focus:ring focus:ring-violet-500/30 active:bg-violet-600 active:border-violet-600"
                    href="{{ route('pricing_rules.create') }}"> <i class="bx bx-plus block text-lg"></i></a>
             </div>
-
         </div>
     </div>
     <div class="grid grid-cols-12 gap-5">
