93be097cc38e9a6013280df4a10c82d554e6d844
ramon-ultimatejetvacations <ramon@ultimatejetvacations.com>
2024-03-06 15:41:31 -0400

    Added config for hbsi credentials that feed from .env

---
+++ Modules/API/Suppliers/HbsiSupplier/Credentials.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\API\Suppliers\HbsiSupplier;
+
+class Credentials
+{
+    public string $username;
+    public string $password;
+    public string $channelIdentifierId;
+}
