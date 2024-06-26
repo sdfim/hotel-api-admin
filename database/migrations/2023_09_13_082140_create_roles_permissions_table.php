e5520c277a4c97588c421b5d64089f90a6fb8445
Shift <shift@laravelshift.com>
2024-06-26 15:04:33 +0000

    Adopt anonymous migrations

--- database/migrations/2023_09_13_082140_create_roles_permissions_table.php
+++ database/migrations/2023_09_13_082140_create_roles_permissions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRolesPermissionsTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -33,4 +33,4 @@ class CreateRolesPermissionsTable extends Migration
     {
         Schema::dropIfExists('roles_permissions');
     }
-}
+};
