6b6c655cd795e35939c021ec8b9dc1961e6c8b30
andrii <kslndr@gmail.com>
2024-06-27 18:12:07 +0300

    performing search cache cleanup without queuing

---
+++ Modules/API/Tools/ClearSearchCacheByBookingItemsTools.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Modules\API\Tools;
+
+use App\Models\ApiBookingItem;
+use App\Models\ApiSearchInspector;
+use Carbon\Carbon;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
+use Modules\API\Controllers\ApiHandlers\HotelApiHandler;
+
+class ClearSearchCacheByBookingItemsTools {
+
+    /**
+     * @param array $bookingItems
+     * @return void
+     */
+    public function clear(array $bookingItems): void
+    {
+        $start = microtime(true);
+
+        $tag = 'pricing_search';
+        $taggedCache = Cache::tags($tag);
+
+        $arr_pricing_search = $taggedCache->get('arr_pricing_search') ?? []; // local dev none cache
+        Log::debug('ClearSearchCacheByBookingItemsTools | $arr_pricing_search : ' . json_encode($arr_pricing_search));
+
+        try {
+            // Process each booking item
+            foreach ($bookingItems as $bookingItem) {
+                // Get booking item data
+                $booking_item_data = ApiBookingItem::where('booking_item', $bookingItem)->first()->booking_item_data;
+                $booking_item_data = json_decode($booking_item_data, true);
+                $room_ids_str = Arr::get($booking_item_data, 'room_id');
+                $hotel_id = Arr::get($booking_item_data, 'hotel_id');
+                $room_ids = explode(';', $room_ids_str);
+
+                // Process each room ID
+                foreach ($room_ids as $room_id) {
+                    // Get search data by hotel ID and room ID
+                    $search_ids_data = ApiSearchInspector::whereIn('search_id', function ($query) use ($hotel_id, $room_id, $arr_pricing_search) {
+                        $query->select('search_id')
+                            ->from('api_booking_items')
+                            ->whereIn('search_id', $arr_pricing_search)
+                            ->whereJsonContains('booking_item_data->hotel_id', $hotel_id)
+                            ->where('booking_item_data->room_id', 'like', $room_id)
+                            ->where('created_at', '>', Carbon::now()->subHour());
+                    })
+                        ->join('channels', 'api_search_inspector.token_id', '=', 'channels.token_id')
+                        ->get(['search_id', 'request', 'api_search_inspector.token_id', 'channels.access_token'])
+                        ->toArray();
+
+                    Log::debug('ClearSearchCacheByBookingItemsTools | $search_ids_data : ' . json_encode($search_ids_data));
+
+                    // Process each search data item
+                    foreach ($search_ids_data as $search_id_data) {
+
+                        if (!in_array($search_id_data['search_id'], $arr_pricing_search)) {
+                            Log::debug('ClearSearchCacheByBookingItemsTools | Cache not found: ' . $search_id_data['search_id']);
+                            continue;
+                        }
+
+                        $currentKeyPricingSearch = $taggedCache->get($search_id_data['search_id']);
+
+                        $taggedCache->forget($search_id_data['search_id']);
+                        $taggedCache->forget($currentKeyPricingSearch);
+
+                        $key = array_search($search_id_data['search_id'], $arr_pricing_search);
+                        if ($key !== false) unset($arr_pricing_search[$key]);
+                        $taggedCache->put('arr_pricing_search', $arr_pricing_search, now()->addMinutes(HotelApiHandler::TTL));
+
+                        Log::debug('ClearSearchCacheByBookingItemsTools | Cache removed: ' . $search_id_data['search_id'],
+                            [
+                                'currentKeyPricingSearch' => $currentKeyPricingSearch,
+                                'arr_pricing_search' => $arr_pricing_search
+                            ]);
+                    }
+                }
+            }
+        } catch (\Exception $e) {
+            Log::error('Error in ClearSearchCacheByBookingItemsTools: ' . $e->getMessage());
+            Log::error('Trace: ' . $e->getTraceAsString());
+        }
+        $end = microtime(true);
+        $execution_time = $end - $start;
+
+        Log::info('ClearSearchCacheByBookingItemsTools | Execution time of clear() in seconds: ' . $execution_time);
+    }
+}
