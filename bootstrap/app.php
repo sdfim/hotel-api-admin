569a7ac2fdfd02dee56f3daffb394c3f1befa79e
ramon-ultimatejetvacations <ramon@ultimatejetvacations.com>
2024-06-30 11:49:08 -0400

    Laravel 11 fixes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Providers\AppServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -15,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->redirectUsersTo(config('app.url').AppServiceProvider::HOME);
+        //$middleware->redirectUsersTo(config('app.url').AppServiceProvider::HOME);
 
         $middleware->append([
             \App\Http\Middleware\SetLocationHeader::class,
