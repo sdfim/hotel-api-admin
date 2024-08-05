0ad70730711eb8ed0ab571f0afbd8e32c089378b
Brayan Bonilla <>
2024-08-05 12:17:52 -0500

    - Save room capacity adults, unknown and children - Send -1 as age when a specific age is not listed in capacity

--- Modules/API/Suppliers/DTO/HBSI/HbsiHotelPricingDto.php
+++ Modules/API/Suppliers/DTO/HBSI/HbsiHotelPricingDto.php
@@ -250,6 +250,8 @@ class HbsiHotelPricingDto
         }
 
         $adults = $children = $infants = $unknown = 0;
+        $childrenAges = [];
+
         foreach ($counts as $age => $count) {
             if ($age < 0)
             {
@@ -257,8 +259,10 @@ class HbsiHotelPricingDto
                 $unknown += $count;
             } elseif ($age < HbsiClient::AGE_INFANT) {
                 $infants += $count;
+                $childrenAges[] = $age;
             } elseif ($age < HbsiClient::AGE_CHILD) {
                 $children += $count;
+                $childrenAges[] = $age;
             } else {
                 $adults += $count;
             }
@@ -360,7 +364,9 @@ class HbsiHotelPricingDto
         $roomResponse->setSupplierRoomName($rate['RoomTypes']['RoomType']['RoomDescription']['@attributes']['Name'] ?? '');
         $roomResponse->setSupplierRoomCode($rateOccupancy);
         $roomResponse->setCapacity([
-            'unknown' => $unknown,
+            'adults'    => $adults - $unknown,
+            'children'  => $childrenAges,
+            'unknown'   => $unknown,
         ]);
 
         $roomResponse->setSupplierBedGroups($rate['bed_groups'] ?? 0);
