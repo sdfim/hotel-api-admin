5c4e76c6d13036e69df45c6a694bd8e76ae1360e
gm2-ujv <sebastian.kaplanski@gm2dev.com>
2024-08-23 14:29:41 -0300

    feat(giata_properties): migration rename giata_properties to properties, add autoincremental id, update code field, add source, property_auto_updates and content_auto_updates

--- database/migrations/2023_09_26_075606_create_giata_properties_table.php
+++ database/migrations/2023_09_26_075606_create_giata_properties_table.php
@@ -11,29 +11,34 @@ return new class extends Migration
      */
     public function up(): void
     {
-        if (! Schema::connection(env('SUPPLIER_CONTENT_DB_CONNECTION', 'mysql_cache'))->hasTable('giata_properties')) {
-            Schema::connection(env('SUPPLIER_CONTENT_DB_CONNECTION', 'mysql_cache'))->create('giata_properties', function (Blueprint $table) {
-                $table->integer('code')->index()->unique();
-                $table->timestamp('last_updated');
-                $table->string('name')->default('')->index();
-                $table->json('chain')->nullable();
-                $table->string('city')->default('')->index();
-                $table->integer('city_id')->nullable()->index();
-                $table->string('locale')->default('');
-                $table->integer('locale_id')->nullable();
-                $table->json('address');
-                $table->string('mapper_address', 255)->nullable()->index();
-                $table->string('mapper_postal_code', 50)->nullable()->index();
-                $table->string('mapper_phone_number', 50)->nullable()->index();
-                $table->json('phone')->nullable();
-                $table->json('position')->nullable();
-                $table->float('latitude', 15, 12)->nullable()->index();
-                $table->float('longitude', 15, 12)->nullable()->index();
-                $table->json('url')->nullable();
-                $table->json('cross_references');
-                $table->timestamps();
-            });
-        }
+      if (! Schema::connection(env('SUPPLIER_CONTENT_DB_CONNECTION', 'mysql_cache'))->hasTable('properties')) {
+        Schema::connection(env('SUPPLIER_CONTENT_DB_CONNECTION', 'mysql_cache'))->create('properties', function (Blueprint $table) {
+            $table->id(); // Auto-increment primary key
+            $table->string('code', 255)->index();
+            $table->timestamp('last_updated');
+            $table->string('name', 191)->default('')->index();
+            $table->json('chain')->nullable();
+            $table->string('city', 191)->default('')->index();
+            $table->integer('city_id')->nullable()->index();
+            $table->string('locale', 191)->default('');
+            $table->integer('locale_id')->nullable();
+            $table->json('address');
+            $table->string('mapper_address', 255)->nullable()->index();
+            $table->string('mapper_postal_code', 50)->nullable()->index();
+            $table->string('mapper_phone_number', 50)->nullable()->index();
+            $table->json('phone')->nullable();
+            $table->json('position')->nullable();
+            $table->float('latitude', 15, 12)->nullable()->index();
+            $table->float('longitude', 15, 12)->nullable()->index();
+            $table->json('url')->nullable();
+            $table->json('cross_references');
+            $table->float('rating')->nullable();
+            $table->enum('source', ['giata', 'custom'])->default('giata');
+            $table->unsignedTinyInteger('property_auto_updates')->default(1);
+            $table->unsignedTinyInteger('content_auto_updates')->default(1);
+            $table->timestamps();
+        });
+      }
     }
 
     /**
@@ -41,6 +46,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::connection(env('SUPPLIER_CONTENT_DB_CONNECTION', 'mysql_cache'))->dropIfExists('giata_properties');
+        Schema::connection(env('SUPPLIER_CONTENT_DB_CONNECTION', 'mysql_cache'))->dropIfExists('properties');
     }
 };
