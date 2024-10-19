bf1deee96d17f5085d3bbb2ce1398ef735d2ccb4
AndriiK <kslndr@gmail.com>
2024-10-19 14:09:26 +0000

    add horizon

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,7 +3,8 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
-    App\Providers\RolesServiceProvider::class,
     App\Providers\PermissionServiceProvider::class,
+    App\Providers\RolesServiceProvider::class,
 ];
