9299417e8ab420d919b79f2924a0ad4de7314fcb
andrii <kslndr@gmail.com>
2024-10-25 11:04:49 +0300

    minor fix

--- database/migrations/2024_10_21_144034_add_fulltext_index_to_properties_table.php
+++ database/migrations/2024_10_21_144034_add_fulltext_index_to_properties_table.php
@@ -4,25 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-    private string $cacheDB;
-
-    public function __construct()
-    {
-        $this->cacheDB = config('database.connections.mysql_cache.database');
-    }
-
+return new class extends Migration
+{
     public function up(): void
     {
         // Check if the 'properties' table exists
-        if (Schema::hasTable('properties')) {
+        if (Schema::connection(env('SUPPLIER_CONTENT_DB_CONNECTION', 'mysql_cache'))->hasTable('properties')) {
 
             // Check if the FULLTEXT index does not already exist
-            $indexExists = DB::select("SHOW INDEX FROM $this->cacheDB.properties WHERE Key_name = 'properties_name_fulltext'");
+            $indexExists = DB::connection(env('SUPPLIER_CONTENT_DB_CONNECTION', 'mysql_cache'))
+                ->select("SHOW INDEX FROM properties WHERE Key_name = 'properties_name_fulltext'");
 
             if (empty($indexExists)) {
                 // Add the FULLTEXT index if it doesn't exist
-                DB::statement("ALTER TABLE `$this->cacheDB.properties` ADD FULLTEXT `properties_name_fulltext` (`name`)");
+                DB::connection(env('SUPPLIER_CONTENT_DB_CONNECTION', 'mysql_cache'))
+                    ->statement("ALTER TABLE properties ADD FULLTEXT `properties_name_fulltext` (`name`)");
             }
         }
     }
@@ -30,13 +26,15 @@ return new class extends Migration {
     public function down(): void
     {
         // Check if the 'properties' table exists
-        if (Schema::hasTable('properties')) {
+        if (Schema::connection(env('SUPPLIER_CONTENT_DB_CONNECTION', 'mysql_cache'))->hasTable('properties')) {
             // Remove the FULLTEXT index if it exists
-            $indexExists = DB::select("SHOW INDEX FROM $this->cacheDB.properties WHERE Key_name = 'properties_name_fulltext'");
+            $indexExists = DB::connection(env('SUPPLIER_CONTENT_DB_CONNECTION', 'mysql_cache'))
+                ->select("SHOW INDEX FROM properties WHERE Key_name = 'properties_name_fulltext'");
 
             if (!empty($indexExists)) {
                 // Drop the FULLTEXT index if it exists
-                DB::statement("ALTER TABLE `$this->cacheDB.properties` DROP INDEX `properties_name_fulltext`");
+                DB::connection(env('SUPPLIER_CONTENT_DB_CONNECTION', 'mysql_cache'))
+                    ->statement("ALTER TABLE properties DROP INDEX `properties_name_fulltext`");
             }
         }
     }
