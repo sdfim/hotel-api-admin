3ab9fc19fe7190bc7050e08b50bc80723719ce68
ihor.didenko <ihor.didenko@askep.net>
2024-11-06 14:27:40 +0200

    fix policies and invite team email

--- app/Policies/HotelImagePolicy.php
+++ app/Policies/HotelImagePolicy.php
@@ -2,6 +2,8 @@
 
 namespace App\Policies;
 
+use App\Policies\Base\BasePolicy;
+
 class HotelImagePolicy extends BasePolicy
 {
     protected static string $prefix = 'hotel_image';
--- app/Policies/ImageGalleryPolicy.php
+++ app/Policies/ImageGalleryPolicy.php
@@ -2,6 +2,8 @@
 
 namespace App\Policies;
 
+use App\Policies\Base\BasePolicy;
+
 class ImageGalleryPolicy extends BasePolicy
 {
     protected static string $prefix = 'image_gallery';
