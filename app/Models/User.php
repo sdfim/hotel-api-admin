9a2e198d95b268a108b3734585d39fbab4d106f6
ihor.didenko <ihor.didenko@askep.net>
2024-11-06 14:08:13 +0200

    set current team after accepting invitation

--- app/Models/User.php
+++ app/Models/User.php
@@ -33,6 +33,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'current_team_id',
     ];
 
     /**
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->append([
             \App\Http\Middleware\SetLocationHeader::class,
             \App\Http\Middleware\RewriteUrls::class,
+            \App\Http\Middleware\SelectTeamAfterAcceptMiddleware::class,
         ]);
 
         $middleware->use([
