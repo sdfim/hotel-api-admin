0b722b37bdb109aa122594cc4084a02ba3205a72
andrii <kslndr@gmail.com>
2024-11-15 13:48:45 +0200

    minor fix

--- resources/views/dashboard/insurance/plans/index.blade.php
+++ resources/views/dashboard/insurance/plans/index.blade.php
@@ -3,7 +3,7 @@
     {{ __('Insurance Plans') }}
 @endsection
 @section('content')
-    <x-page-title title="Insurance Providers" pagetitle="index"/>
+    <x-page-title title="Insurance Plans" pagetitle="index"/>
     <div class="grid grid-cols-12 gap-5">
         <div class="col-span-12">
             <div class="card dark:bg-zinc-800 dark:border-zinc-600">
