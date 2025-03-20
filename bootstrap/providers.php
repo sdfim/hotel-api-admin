fc5030de26a7e99f78e7de77f8875a907f6b15cf
Brayan Bonilla <>
2025-03-20 17:03:31 -0500

    fx

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,7 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
-    App\Providers\HorizonServiceProvider::class,
+    //App\Providers\HorizonServiceProvider::class,
     App\Providers\JetstreamServiceProvider::class,
     App\Providers\PermissionServiceProvider::class,
     App\Providers\RolesServiceProvider::class,
