632cf4688353311b0ec1d9b9f998c44fb2fca12d
andrii <kslndr@gmail.com>
2025-04-10 16:23:12 +0300

    fix: PropertiesTable value longitude

--- app/Livewire/PropertiesTable.php
+++ app/Livewire/PropertiesTable.php
@@ -169,9 +169,9 @@ class PropertiesTable extends Component implements HasForms, HasTable
                     TextInput::make('longitude')
                         ->label('Longitude')
                         ->numeric()
-                        ->minValue(-90)
-                        ->maxValue(90)
-                        ->disabled(! $isEditable),
+                        ->minValue(-180)
+                        ->maxValue(180)
+                        ->disabled(!$isEditable),
 
                     TextInput::make('mapper_phone_number')
                         ->label('Phone')
