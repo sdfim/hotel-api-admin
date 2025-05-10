cbb9805b6e6dd8aba65fa28b9897c58658f774fe
Brayan Bonilla <>
2025-05-10 11:51:36 -0500

    opcache reset

--- infrastructure/docker/php-8.2/entrypoint.sh
+++ infrastructure/docker/php-8.2/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 cd /var/www/html
+
+php artisan opcache:reset
+
 php artisan optimize:clear
+
 echo "Starting Laravel Optimize..." >&2
 php artisan optimize
 echo "Finished Laravel Optimize..." >&2
