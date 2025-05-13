3c2fc488e97eafc0895a69c60b87bb938c203ff0
Brayan Bonilla <>
2025-05-13 12:39:34 -0500

    remove optimize temp

--- infrastructure/docker/php-8.2/entrypoint.sh
+++ infrastructure/docker/php-8.2/entrypoint.sh
@@ -4,25 +4,12 @@ cd /var/www/html
 
 composer dump-autoload --optimize
 
+# Reset caches at runtime when env vars are available
+php artisan optimize:clear
+
 # Reset OPcache if available
 php -r "if (function_exists('opcache_reset')) { opcache_reset(); echo 'OPcache reset.'; }"
-
-# Reset caches at runtime when env vars are available
-php artisan clear-compiled
-php artisan optimize:clear
-php artisan cache:clear
-php artisan view:clear
-php artisan event:clear
-php artisan config:clear # Crucial if .env could have changed
-php artisan route:clear
 
 echo "Starting Laravel Optimize..." >&2
-
-php artisan optimize
-
+#php artisan optimize
 echo "Finished Laravel Optimize..." >&2
-
-sudo systemctl restart apache2 # Common for Apache setups
-
-# Reset OPcache if available
-php -r "if (function_exists('opcache_reset')) { opcache_reset(); echo 'OPcache reset.'; }"
