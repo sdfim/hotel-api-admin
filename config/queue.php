fe249182f888dcdb69c8afb5ce913019f3febb7d
andrii <kslndr@gmail.com>
2025-06-25 22:02:53 +0300

    fix: queue config

--- config/queue.php
+++ config/queue.php
@@ -79,7 +79,7 @@ return [
             'hosts' => [
                 [
                     'host' => env('RABBITMQ_HOST', '127.0.0.1'),
-                    'port' => env('RABBITMQ_PORT', 5672),
+                    'port' => env('RABBITMQ_PORT', 5671),
                     'user' => env('RABBITMQ_USER', 'guest'),
                     'password' => env('RABBITMQ_PASSWORD', 'guest'),
                     'vhost' => env('RABBITMQ_VHOST', '/'),
