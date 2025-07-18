43769fbd7a06daed4647b42c406d05e7a7d69c1f
andrii <kslndr@gmail.com>
2025-07-18 16:45:06 +0300

    feat: SI

--- resources/views/layouts/master-without-nav.blade.php
+++ resources/views/layouts/master-without-nav.blade.php
@@ -3,9 +3,9 @@
 
 <head>
     <meta charset="utf-8"/>
-    <title>@yield('title') - Laravel Admin & Dashboard Template</title>
+    <title>@yield('title') - TerraMare</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-    <meta content="Tailwind Admin & Dashboard Template" name="description"/>
+    <meta content="TerraMare" name="description"/>
     <meta content="" name="TerraMare"/>
     <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 
