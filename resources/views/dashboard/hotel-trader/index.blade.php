0cbadf7a9ec1d84fa99d784abed36a269ac57431
andrii <kslndr@gmail.com>
2025-07-30 16:33:26 +0300

    feat: add HotelTraderContentHotel to UI

--- resources/views/dashboard/hotel-trader/index.blade.php
+++ resources/views/dashboard/hotel-trader/index.blade.php
@@ -8,7 +8,7 @@
 
     <div class="grid grid-cols-12 gap-5">
         <div class="col-span-12">
-            @livewire('hotel-trader-content-table')
+            @livewire('hotel-trader-content-hotel-table')
         </div>
     </div>
 @endsection
--- resources/views/livewire/hotel-trader-content-table.blade.php
+++ resources/views/livewire/hotel-trader-content-table.blade.php
@@ -1,4 +1,3 @@
 <div>
-    <!-- Hotel Trader Content Table Component -->
-    <p>Hotel Trader Content Table will be displayed here.</p>
+    {{ $this->table }}
 </div>
