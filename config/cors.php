c4372784ea4209530f06306e9dc72fe92c3bf00b
andrii <kslndr@gmail.com>
2025-09-03 16:02:16 +0300

    feat: UP cors

--- config/cors.php
+++ config/cors.php
@@ -2,7 +2,18 @@
 
 return [
 
-    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '')),
     'paths' => ['api/*', 'admin/*', 'docs/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => explode(',', env('CORS_ALLOWED_ORIGINS', '')),
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
 
 ];
