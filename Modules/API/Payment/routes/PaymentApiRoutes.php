b6b45acf253a4d7dfad77ddfab187b734c5897a9
andrii <kslndr@gmail.com>
2025-09-12 18:52:36 +0300

    feat: AirwallexClient

--- Modules/API/Payment/routes/PaymentApiRoutes.php
+++ Modules/API/Payment/routes/PaymentApiRoutes.php
@@ -8,7 +8,7 @@ class PaymentApiRoutes
 {
     public static function routes(): void
     {
-        Route::prefix('payment')->group(function () {
+        Route::middleware('auth:sanctum')->prefix('payment')->group(function () {
             Route::post('create', [\Modules\API\Payment\Controllers\AirwallexProxyController::class, 'createPaymentIntent']);
             Route::get('payment-intent/{id}', [\Modules\API\Payment\Controllers\AirwallexProxyController::class, 'retrievePaymentIntent']);
             // Future Airwallex routes can be added here
