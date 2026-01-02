f750acc3b04984ae558e8c1033db63f95d5c4132
andrii.kyselov <kslndr@gmail.com>
2026-01-02 13:13:37 +0200

    refactoring: s12; update HotelApiHandler p2

--- Modules/API/Controllers/ApiHandlers/HotelApiHandler.php
+++ Modules/API/Controllers/ApiHandlers/HotelApiHandler.php
@@ -326,35 +326,46 @@ class HotelApiHandler extends BaseController implements ApiHandlerInterface
                     Cache::put($key, gzcompress(json_encode($$variableName)), now()->addMinutes(10));
                 }
 
-                $isTestScenario = $request->input('is_test_scenario', false);
-                if ($isTestScenario) {
-                    SaveSearchInspectorByCacheKey::dispatchSync($searchInspector, $cacheKeys);
-                    MemoryLogger::log('SaveSearchInspectorByCacheKey');
+                // this approach is more memory-efficient.
+                SaveSearchInspectorByCacheKey::dispatch($searchInspector, $cacheKeys);
 
-                    if (! empty($bookingItems)) {
-                        foreach ($bookingItems as $items) {
-                            SaveBookingItems::dispatchSync($items);
-                        }
-                    }
-                } else {
-                    $jobs = [new SaveSearchInspectorByCacheKey($searchInspector, $cacheKeys)];
-                    if (! empty($bookingItems)) {
-                        foreach ($bookingItems as $items) {
-                            $jobs[] = new SaveBookingItems($items);
-                        }
-                    }
-                    $pendingChain = Bus::chain($jobs);
-                    // If we are inside a transaction
-                    if (DB::transactionLevel() > 0) {
-                        // Run the chain only after a successful COMMIT
-                        DB::afterCommit(function () use ($pendingChain) {
-                            $pendingChain->dispatch();
-                        });
-                    } else {
-                        // No transaction, run immediately
-                        $pendingChain->dispatch();
+                MemoryLogger::log('SaveSearchInspectorByCacheKey');
+
+                if (! empty($bookingItems)) {
+                    foreach ($bookingItems as $items) {
+                        SaveBookingItems::dispatch($items);
                     }
                 }
+
+//                $isTestScenario = $request->input('is_test_scenario', false);
+//                if ($isTestScenario) {
+//                    SaveSearchInspectorByCacheKey::dispatchSync($searchInspector, $cacheKeys);
+//                    MemoryLogger::log('SaveSearchInspectorByCacheKey');
+//
+//                    if (! empty($bookingItems)) {
+//                        foreach ($bookingItems as $items) {
+//                            SaveBookingItems::dispatchSync($items);
+//                        }
+//                    }
+//                } else {
+//                    $jobs = [new SaveSearchInspectorByCacheKey($searchInspector, $cacheKeys)];
+//                    if (! empty($bookingItems)) {
+//                        foreach ($bookingItems as $items) {
+//                            $jobs[] = new SaveBookingItems($items);
+//                        }
+//                    }
+//                    $pendingChain = Bus::chain($jobs);
+//                    // If we are inside a transaction
+//                    if (DB::transactionLevel() > 0) {
+//                        // Run the chain only after a successful COMMIT
+//                        DB::afterCommit(function () use ($pendingChain) {
+//                            $pendingChain->dispatch();
+//                        });
+//                    } else {
+//                        // No transaction, run immediately
+//                        $pendingChain->dispatch();
+//                    }
+//                }
 
                 $res = $request->input('supplier_data') == 'true' ? $content : $clientContent;
                 $res['search_id'] = $search_id;
