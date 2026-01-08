1806d53216c01f3b48ca6a48fac754932278aa99
andrii.kyselov <kslndr@gmail.com>
2026-01-08 16:56:50 +0200

    feat: update Inspectors

--- resources/views/dashboard/search-inspector/column/error-data.blade.php
+++ resources/views/dashboard/search-inspector/column/error-data.blade.php
@@ -13,7 +13,7 @@
                 class="relative overflow-hidden text-left transition-all transform bg-white rounded-lg shadow-xl dark:bg-zinc-600">
                 <div class="bg-white dark:bg-zinc-700">
                     <div class="flex items-center p-4 border-b rounded-t border-gray-50 dark:border-zinc-600">
-                        <h3 class="text-xl font-semibold text-gray-900 dark:text-gray-100 ">
+                        <h3 class="text-xl font-semibold text-gray-100 dark:text-gray-100 ">
                             {{ $getRecord()->search_id }}
                         </h3>
                         <button onClick="closeModalError('{{ $getRecord()->search_id }}','request')"
